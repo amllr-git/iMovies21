5a7972a8ef3edada7543b3bcacdd2e5c877b613c
mllr <mllr@mllrs-MacBook-Pro.local>
2025-08-05 15:49:15 +0800

    add state function

---
+++ src/pages/Home.jsx
@@ -0,0 +1,41 @@
+import MovieCard from "../components/MovieCard"
+import { useState } from "react";
+
+function Home() {
+    const [searchQuery, setSearchQuery] = useState("");
+
+    const movies = [
+        { id: 1, title: "Fantastic Four: First Step", release_date: "2025" },
+        { id: 2, title: "Blade Runner 2049", release_date: "2017" },
+        { id: 3, title: "Spiderman: No Way Home", release_date: "2021" },
+        { id: 4, title: "The Batman", release_date: "2022" },
+    ];
+
+    const handleSearch = (e) => {
+        e.preventDefault();
+        alert(searchQuery);
+    };
+
+    return (
+        <div className="Home">
+            <form onSubmit={handleSearch} className="search-form">
+                <input
+                    type="text"
+                    placeholder="Search for movies..."
+                    className="search-input"
+                    value={searchQuery}
+                    onChange={(e) => setSearchQuery(e.target.value)}
+                />
+                <button type="submit" className="search-button">Search</button>
+            </form>
+
+            <div className="movies-grid">
+                {movies.map((movie) => (
+                    <MovieCard movie={movie} key={movie.id} />
+                ))}
+            </div>
+        </div>
+    );
+}
+
+export default Home
